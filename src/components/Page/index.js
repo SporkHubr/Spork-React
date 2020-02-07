780959bd70254042e59d7f437a61ef5813520ec2
aspirin <b.bogomazov@gmail.com>
2020-02-07 22:34:15 +0100

    Replace all to styled components & fix eslint problems with lazyload

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,1 +1,25 @@
-export { default } from './Page';
+/* eslint-disable no-undef */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
+
+const Page = (props) => {
+  const { title, children, className } = props;
+
+  return (
+    <div className={className}>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      {children}
+    </div>
+  );
+};
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+export default Page;
